e487b7649b90aace826f878c54167db7b0dd3b9f
Usuario <Usuario@Fa-PC>
2017-11-08 01:29:52 -0200

    Lista Forros e Lista Paredes

--- src/cmd/testes/TesteCadApenasTelefone.java
+++ src/cmd/testes/TesteCadApenasTelefone.java
@@ -10,8 +10,6 @@ import cmd.entidade.Cliente;
 import cmd.entidade.Endereco;
 import cmd.entidade.Telefone;
 import cmd.entidade.TelefoneId;
-import cmd.novo.controle.CadClientesControle;
-import cmd.novo.controle.CadTelefoneControle;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -78,101 +76,101 @@ public class TesteCadApenasTelefone {
     }
 
     private static void testeCadastroTelefoneComCliente() {
-        CadClientesControle cadCliC = new CadClientesControle();
-
-        cadCliC = new CadClientesControle();
-        Cliente cli = new Cliente();
-        Endereco end = new Endereco();
-        //PessoaJuridica pJur = new PessoaJuridica();
-
-        HashSet<Telefone> tels = new HashSet<>();
-
-        end.setCodEndereco(3);
-        end.setBairro("Bairro_5_Tes");
-        end.setCep("04233-010");
-        end.setCidade("Cidade_5_Tes");
-        end.setComplemento("Complemento_5_Tes");
-        end.setLogradouro("Logradouro_5_Tes");
-        end.setNumero("10");
-        end.setUf("55");
-        end.setXdead(false);
-
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        Date dataDate = new Date();
-        String dS = "11/01/2011";
-        try {
-            dataDate = formato.parse(dS);
-            System.out.println(dataDate);
-        } catch (ParseException ex) {
-            System.out.println(ex);
-        }
-
-        /*
-         78.846.292/0001-75
-         52.852.698/0001-77
-         79.664.724/0001-90
-         */
-//        pJur.setCnpj("78.846.292/0001-75");
-//        pJur.setDataFundacao(dataDate);
-//        pJur.setRamoAtuacao("Ramo_5_Atuac");
-//        pJur.setRazaoSocial("Razao_5_Soci");
-//        pJur.setXdead(false);
-        Telefone tel = new Telefone();
-        TelefoneId telId = new TelefoneId();
-
-        telId.setNumero("(11)1000-1017");
-        telId.setCodCliente(9);//IMPORTANTE
-        tel.setId(telId);
-        tel.setCliente(null);
-        tel.setXdead(true);
-
-        tels.add(tel);
-
-        tel = new Telefone();
-        telId = new TelefoneId();
-        telId.setNumero("(11)1000-1018");
-        telId.setCodCliente(9);//IMPORTANTE
-        tel.setId(telId);
-        tel.setCliente(null);
-        tel.setXdead(true);
-
-        tels.add(tel);
-
-        tel = new Telefone();
-        telId = new TelefoneId();
-        telId.setNumero("(11)1000-1019");
-        telId.setCodCliente(9);//IMPORTANTE
-        tel.setId(telId);
-        tel.setCliente(null);
-        tel.setXdead(true);
-
-        tels.add(tel);
-
-        Iterator<Telefone> iterator = tels.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(">>>>>>>>>>>>> " + iterator.next().getId().getNumero());
-
-        }
-
-        cli.setCodCliente(null);
-        cli.setOrcamentos(null);
-        cli.setDataInscricao(dataDate);
-        cli.setEndereco(end);
-        //cli.setEndereco(null);
-        //cli.setPessoaJuridica(pJur);
-        cli.setPessoaJuridica(null);
-        cli.setTelefones(tels);
-        cli.setXdead(false);
-
-        if (cadCliC.CadastrarClientePJuridicoEnderecoTelefone(cli) == true) {
-            JOptionPane.showMessageDialog(null, "Cadastrado Varios");
-
-        }
+//       
+//
+//        cadCliC = new CadClientesControle();
+//        Cliente cli = new Cliente();
+//        Endereco end = new Endereco();
+//        //PessoaJuridica pJur = new PessoaJuridica();
+//
+//        HashSet<Telefone> tels = new HashSet<>();
+//
+//        end.setCodEndereco(3);
+//        end.setBairro("Bairro_5_Tes");
+//        end.setCep("04233-010");
+//        end.setCidade("Cidade_5_Tes");
+//        end.setComplemento("Complemento_5_Tes");
+//        end.setLogradouro("Logradouro_5_Tes");
+//        end.setNumero("10");
+//        end.setUf("55");
+//        end.setXdead(false);
+//
+//        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+//        Date dataDate = new Date();
+//        String dS = "11/01/2011";
+//        try {
+//            dataDate = formato.parse(dS);
+//            System.out.println(dataDate);
+//        } catch (ParseException ex) {
+//            System.out.println(ex);
+//        }
+//
+//        /*
+//         78.846.292/0001-75
+//         52.852.698/0001-77
+//         79.664.724/0001-90
+//         */
+////        pJur.setCnpj("78.846.292/0001-75");
+////        pJur.setDataFundacao(dataDate);
+////        pJur.setRamoAtuacao("Ramo_5_Atuac");
+////        pJur.setRazaoSocial("Razao_5_Soci");
+////        pJur.setXdead(false);
+//        Telefone tel = new Telefone();
+//        TelefoneId telId = new TelefoneId();
+//
+//        telId.setNumero("(11)1000-1017");
+//        telId.setCodCliente(9);//IMPORTANTE
+//        tel.setId(telId);
+//        tel.setCliente(null);
+//        tel.setXdead(true);
+//
+//        tels.add(tel);
+//
+//        tel = new Telefone();
+//        telId = new TelefoneId();
+//        telId.setNumero("(11)1000-1018");
+//        telId.setCodCliente(9);//IMPORTANTE
+//        tel.setId(telId);
+//        tel.setCliente(null);
+//        tel.setXdead(true);
+//
+//        tels.add(tel);
+//
+//        tel = new Telefone();
+//        telId = new TelefoneId();
+//        telId.setNumero("(11)1000-1019");
+//        telId.setCodCliente(9);//IMPORTANTE
+//        tel.setId(telId);
+//        tel.setCliente(null);
+//        tel.setXdead(true);
+//
+//        tels.add(tel);
+//
+//        Iterator<Telefone> iterator = tels.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(">>>>>>>>>>>>> " + iterator.next().getId().getNumero());
+//
+//        }
+//
+//        cli.setCodCliente(null);
+//        cli.setOrcamentos(null);
+//        cli.setDataInscricao(dataDate);
+//        cli.setEndereco(end);
+//        //cli.setEndereco(null);
+//        //cli.setPessoaJuridica(pJur);
+//        cli.setPessoaJuridica(null);
+//        cli.setTelefones(tels);
+//        cli.setXdead(false);
+//
+//        if (cadCliC.CadastrarClientePJuridicoEnderecoTelefone(cli) == true) {
+//            JOptionPane.showMessageDialog(null, "Cadastrado Varios");
+//
+//        }
 
     }
 
     private static void testeCadastroTelefoneUnitario() {
-        CadTelefoneControle cTelc = new CadTelefoneControle();
+      
         Telefone tel = new Telefone();
         TelefoneId tId = new TelefoneId();
 //=======================================
@@ -186,11 +184,11 @@ public class TesteCadApenasTelefone {
         tel.setCliente(null);
         tel.setXdead(true);
 
-        if (cTelc.Cadastrar(tel) == true) {
-            System.out.println("tel Cadastrado");
-        } else {
-            System.out.println("tel NÃO cadastrado");
-        }
+//        if (cTelc.Cadastrar(tel) == true) {
+//            System.out.println("tel Cadastrado");
+//        } else {
+//            System.out.println("tel NÃO cadastrado");
+//        }
 
     }
 }
